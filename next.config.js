5285e53ce6770cb4613ce3b57d30e84733cb4060
hammadtariq838 <24100267@lums.edu.pk>
2024-07-17 23:57:37 +0500

    feat: added typescript + lint checking back

--- next.config.js
+++ next.config.js
@@ -7,12 +7,12 @@ const nextConfig = {
       }
     ],
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // }
 }
 
 module.exports = nextConfig

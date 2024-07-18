fb2f1976ebb6eea966dd4c37676ecab631ef9033
hammadtariq838 <24100267@lums.edu.pk>
2024-07-18 11:21:14 +0500

    chore: user protected route component

---
+++ components/auth/user-route.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { useCurrentRole } from "@/hooks/use-current-role";
+import { useRouter } from "next/navigation";
+
+const AdminRoute = () => {
+  const role = useCurrentRole();
+  const router = useRouter();
+
+  if (role !== 'USER') {
+    router.push('/unauthorized')
+    return null;
+  }
+
+  return null;
+}
+
+export default AdminRoute;

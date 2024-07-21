7b71f6ea1f055d4edbf23f23db3f68600b533fd1
hammadtariq838 <24100267@lums.edu.pk>
2024-07-21 15:14:25 +0500

    chore: created contact form component to allow only javascript that is needed

--- app/(marketing)/about-us/page.tsx
+++ app/(marketing)/about-us/page.tsx
@@ -1,55 +1,7 @@
-'use client';
-
-import { addQuery } from "@/actions/add-query";
-import { TextInput } from "@/components/TextInput";
-import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
-import { Textarea } from "@/components/ui/textarea";
-import { ContactQuerySchema } from "@/schemas/contactQuery";
-import { zodResolver } from "@hookform/resolvers/zod";
+import ContactForm from "@/components/ContactForm";
 import Image from "next/image";
-import { useTransition } from "react";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { z } from "zod";
 
 const ContactSection = () => {
-  const [isPending, startTransition] = useTransition();
-
-  const form = useForm<z.infer<typeof ContactQuerySchema>>({
-    resolver: zodResolver(ContactQuerySchema),
-    defaultValues: {
-      name: '',
-      email: '',
-      subject: '',
-      message: ''
-    }
-  });
-
-  const { getFieldState, formState } = form;
-  const fieldState = getFieldState("message", formState);
-
-  const onSubmit = (values: z.infer<typeof ContactQuerySchema>) => {
-    console.log('values', values)
-
-    startTransition(() => {
-      addQuery(values)
-        .then((data) => {
-          if (data.success)
-            toast.success(data.success);
-          if (data.error)
-            toast.error(data.error);
-          form.reset({
-            name: '', email: '', subject: '', message: ''
-          });
-        })
-        .catch((error) => {
-          console.error(error);
-          toast.error("An error occurred. Please try again.");
-        })
-    });
-  };
-
   return (
     <section className="bg-white xl:bg-[#FEFAEB] w-full">
       <div className="mx-auto md:max-w-3xl xl:max-w-6xl px-5 py-16 space-y-16">
@@ -59,42 +11,7 @@ const ContactSection = () => {
         <div className="self-center px-12 py-8 bg-white rounded-xl border border-solid shadow-sm border-slate-300 mb-10">
           <div className="flex items-center justify-between gap-5 max-md:flex-col max-md:gap-0">
             <div className="flex flex-col w-1/2 max-md:ml-0 max-md:w-full">
-              <Form {...form}>
-                <form
-                  onSubmit={form.handleSubmit(onSubmit)}
-                  className="space-y-4"
-                >
-                  <div className="space-y-2">
-                    <TextInput id="name" label="Name" required />
-                    <TextInput id="email" type="email" label="Email" required />
-                    <TextInput id="subject" label="Subject" required />
-                    <FormField
-                      control={form.control}
-                      name="message"
-                      render={({ field }) => (
-                        <FormItem className="w-full">
-                          <FormLabel>Message<span className="text-destructive">*</span></FormLabel>
-                          <FormControl>
-                            <Textarea
-                              {...field}
-                              className={fieldState.error ? "border-destructive" : ""}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  </div>
-                  <Button
-                    type="submit"
-                    variant="brand"
-                    className="w-full"
-                    disabled={isPending}
-                  >
-                    Submit
-                  </Button>
-                </form>
-              </Form>
+              <ContactForm />
             </div>
             <div className="hidden md:flex flex-col text-sm leading-5 text-center text-slate-950 max-md:mt-10 max-w-[374px]">
               <div className="relative w-[374px] h-[220px]">

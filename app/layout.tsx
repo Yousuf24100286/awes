bb0c5a2b1362986b9ce1b9c82ac601623e10df6d
Yousuf Khan <24100286@lums.edu.pk>
2024-09-25 07:35:33 +0500

    add whatsapp icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Toaster } from "@/components/ui/sonner";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { extractRouterConfig } from "uploadthing/server";
 import { ourFileRouter } from "@/app/api/uploadthing/core";
+import Image from 'next/image';
 
 export const metadata: Metadata = {
   title: 'AWES',
@@ -27,6 +28,11 @@ export default async function RootLayout({
           <Toaster position='top-center' richColors />
           {children}
           <script defer src="https://cloud.umami.is/script.js" data-website-id="b9b3e225-6122-465e-a11a-86840f65e25e" />
+          <div className='fixed bottom-0 right-0 p-4'>
+            <a href="https://wa.me/19294138699" target='_blank'>
+              <Image src="/whatsapp-icon.png" alt="whatsapp icon" width={60} height={60} />
+            </a>
+          </div>
         </body>
       </html>
     </SessionProvider>
